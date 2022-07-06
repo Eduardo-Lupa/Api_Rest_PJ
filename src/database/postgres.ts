08bd3c82a05076e69876d6bfed15377cec4e7020
Eduardo-Lupa <eduardozapera@gmail.com>
2022-07-05 21:55:55 -0300

    comentarios extras e criacao de scripts

--- src/database/postgres.ts
+++ src/database/postgres.ts
@@ -1,13 +1,14 @@
 import { Pool } from "pg";
 
-// configuracao da conexao do DB
+// nao utilizacao do dotenv por nao saber se funcionaria para vcs, mas aprendi o uso
+// configuracao da conexao do DB teste elephantSQL
 const USER = "uarznscs";
 const PASSWD = "4z-H3Ca2S4aPjT2TRk9bAFko3Dljsy5a";
 const LOCAL = "tuffi.db.elephantsql.com";
 const DefDB = "uarznscs";
 
 
- const connectionString = `postgres://${USER}:${PASSWD}@${LOCAL}/${DefDB}`;
+const connectionString = `postgres://${USER}:${PASSWD}@${LOCAL}/${DefDB}`;
 
 const db = new Pool({ connectionString });
 
